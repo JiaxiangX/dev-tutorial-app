46e968dee3003c2b3d06ad4568febc644594c22a
Law Yue Jun <yue_jun_1@hotmail.com>
2018-06-12 11:15:00 +0800

    Fresh copy and change in md

--- myinfo-tutorial-app/routes/index.js
+++ myinfo-tutorial-app/routes/index.js
@@ -80,40 +80,7 @@ router.post('/getPersonData', function(req, res, next) {
   // **** CALL TOKEN API ****
   // Call the Token API (with the authorisation code)
   // t2step2 PASTE CODE BELOW
-  request = createTokenRequest(code);
-  request
-    .buffer(true)
-    .end(function(callErr, callRes) {
-      if (callErr) {
-        // ERROR
-        console.log("Error from Token API:".red);
-        console.log(callErr.status);
-        console.log(callErr.response.req.res.text);
-        res.jsonp({
-          status: "ERROR",
-          msg: callErr
-        });
-      } else {
-        // SUCCESSFUL
-        var data = {
-          body: callRes.body,
-          text: callRes.text
-        };
-        console.log("Response from Token API:".green);
-        console.log(JSON.stringify(data.body));
 
-        var accessToken = data.body.access_token;
-        if (accessToken == undefined || accessToken == null) {
-          res.jsonp({
-            status: "ERROR",
-            msg: "ACCESS TOKEN NOT FOUND"
-          });
-        }
-
-        // everything ok, call person API
-        callPersonAPI(accessToken, res);
-      }
-    });
   // t2step2 END PASTE CODE
 
 });
@@ -121,106 +88,13 @@ router.post('/getPersonData', function(req, res, next) {
 function callPersonAPI(accessToken, res) {
   // validate and decode token to get UINFIN
   // t2step4 PASTE CODE BELOW
-  var decoded = securityHelper.verifyJWS(accessToken, _publicCertContent);
-  if (decoded == undefined || decoded == null) {
-    res.jsonp({
-      status: "ERROR",
-      msg: "INVALID TOKEN"
-    })
-  }
 
-  console.log("Decoded Access Token:".green);
-  console.log(JSON.stringify(decoded));
-
-  var uinfin = decoded.sub;
-  if (uinfin == undefined || uinfin == null) {
-    res.jsonp({
-      status: "ERROR",
-      msg: "UINFIN NOT FOUND"
-    });
-  }
   // t2step4 END PASTE CODE
 
   // **** CALL PERSON API ****
   // Call Person API using accessToken
   // t2step5 PASTE CODE BELOW
-  var request = createPersonRequest(uinfin, accessToken);
 
-  // Invoke asynchronous call
-  request
-    .buffer(true)
-    .end(function(callErr, callRes) {
-      if (callErr) {
-        console.log("Error from Person API:".red);
-        console.log(callErr.status);
-        console.log(callErr.response.req.res.text);
-        res.jsonp({
-          status: "ERROR",
-          msg: callErr
-        });
-      } else {
-        // SUCCESSFUL
-        var data = {
-          body: callRes.body,
-          text: callRes.text
-        };
-
-        var personData = data.text;
-        if (personData == undefined || personData == null) {
-          res.jsonp({
-            status: "ERROR",
-            msg: "PERSON DATA NOT FOUND"
-          });
-        }
-        else {
-          if (_authLevel == "L0") {
-            personData = JSON.parse(personData);
-            personData.uinfin = uinfin; // add the uinfin into the data to display on screen
-
-            console.log("Person Data :".green);
-            console.log(JSON.stringify(personData));
-            // successful. return data back to frontend
-            res.jsonp({
-              status: "OK",
-              text: personData
-            });
-
-          }
-          else if(_authLevel == "L2"){
-            console.log("Response from Person API:".green);
-            console.log(personData);
-            //t3step3 PASTE CODE BELOW
-            // header.encryptedKey.iv.ciphertext.tag
-            var jweParts = personData.split(".");
-
-            securityHelper.decryptJWE(jweParts[0], jweParts[1], jweParts[2], jweParts[3], jweParts[4], _privateKeyContent)
-              .then(personData => {
-                if (personData == undefined || personData == null)
-                  res.jsonp({
-                    status: "ERROR",
-                    msg: "INVALID DATA OR SIGNATURE FOR PERSON DATA"
-                  });
-                personData.uinfin = uinfin; // add the uinfin into the data to display on screen
-
-                console.log("Person Data (Decoded/Decrypted):".green);
-                console.log(JSON.stringify(personData));
-                // successful. return data back to frontend
-                res.jsonp({
-                  status: "OK",
-                  text: personData
-                });
-              })
-              .catch(error => {
-                console.error("Error with decrypting JWE: %s".red, error);
-              })
-            //t3step3 END PASTE CODE
-          }
-          else {
-            throw new Error("Unknown Auth Level");
-          }
-        } // end else
-      }
-    }); // end asynchronous call
   // t2step5 END PASTE CODE
 
 }
@@ -237,66 +111,7 @@ function createTokenRequest(code) {
 
   // preparing the request with header and parameters
   // t2step3 PASTE CODE BELOW
-  // assemble params for Token API
-  var strParams = "grant_type=authorization_code" +
-    "&code=" + code +
-    "&redirect_uri=" + _redirectUrl +
-    "&client_id=" + _clientId +
-    "&client_secret=" + _clientSecret;
-  var params = querystring.parse(strParams);
 
-
-  // assemble headers for Token API
-  var strHeaders = "Content-Type=" + contentType + "&Cache-Control=" + cacheCtl;
-  var headers = querystring.parse(strHeaders);
-
-  // Sign request and add Authorization Headers
-  // t3step2a PASTE CODE BELOW
-  var authHeaders = securityHelper.generateAuthorizationHeader(
-    _tokenApiUrl,
-    params,
-    method,
-    contentType,
-    _authLevel,
-    _clientId,
-    _privateKeyContent,
-    _clientSecret,
-    _realm
-  );
-
-  if (!_.isEmpty(authHeaders)) {
-    _.set(headers, "Authorization", authHeaders);
-  }
-  // t3step2a END PASTE CODE
-
-
-  console.log("Request Header for Token API:".green);
-  console.log(JSON.stringify(headers));
-
-  var request = restClient.post(_tokenApiUrl);
-
-  // Set headers
-  if (!_.isUndefined(headers) && !_.isEmpty(headers))
-    request.set(headers);
-
-  // Set Params
-  if (!_.isUndefined(params) && !_.isEmpty(params))
-    request.send(params);
-  // t3step2a END PASTE CODE
-
-
-  console.log("Request Header for Token API:".green);
-  console.log(JSON.stringify(headers));
-
-  var request = restClient.post(_tokenApiUrl);
-
-  // Set headers
-  if (!_.isUndefined(headers) && !_.isEmpty(headers))
-    request.set(headers);
-
-  // Set Params
-  if (!_.isUndefined(params) && !_.isEmpty(params))
-    request.send(params);
   // t2step3 END PASTE CODE
 
   console.log("Sending Token Request >>>".green);
@@ -314,50 +129,7 @@ function createPersonRequest(uinfin, validToken) {
   var request = null;
   // assemble params for Person API
   // t2step6 PASTE CODE BELOW
-  var strParams = "client_id=" + _clientId +
-    "&attributes=" + _attributes;
-  var params = querystring.parse(strParams);
 
-  // assemble headers for Person API
-  var strHeaders = "Cache-Control=" + cacheCtl;
-  var headers = querystring.parse(strHeaders);
-  var authHeaders;
-
-  // Sign request and add Authorization Headers
-  // t3step2b PASTE CODE BELOW
-  authHeaders = securityHelper.generateAuthorizationHeader(
-    url,
-    params,
-    method,
-    "", // no content type needed for GET
-    _authLevel,
-    _clientId,
-    _privateKeyContent,
-    _clientSecret,
-    _realm
-  );
-  // t3step2b END PASTE CODE
-  if (!_.isEmpty(authHeaders)) {
-    _.set(headers, "Authorization", authHeaders + ",Bearer " + validToken);
-  }
-  else {
-    // NOTE: include access token in Authorization header as "Bearer " (with space behind)
-      _.set(headers, "Authorization", "Bearer " + validToken);
-  }
-
-  console.log("Request Header for Person API:".green);
-  console.log(JSON.stringify(headers));
-
-  // invoke token API
-  var request = restClient.get(url);
-
-  // Set headers
-  if (!_.isUndefined(headers) && !_.isEmpty(headers))
-    request.set(headers);
-
-  // Set Params
-  if (!_.isUndefined(params) && !_.isEmpty(params))
-    request.query(params);
   // t2step6 END PASTE CODE
   console.log("Sending Person Request >>>".green);
   return request;
--- tutorial-readme.md
+++ tutorial-readme.md
@@ -34,8 +34,9 @@ Access the following URL `http://localhost:3001` from your browser
 ![myinfo-tutorial-app](myinfo-tutorial-app-landing.jpg)
 
 If you can see the page above, it means your setup is complete and you are ready for the tutorial!
-Please note that this is a blank tutorial app, you have to follow the tutorial to see the expected results.
->Please contact the facilitators if you are having difficulties with the setup
+
+>Please note that this is a blank tutorial app, you have to follow the tutorial to see the expected results.    
+>Please contact the facilitators if you are having difficulties with the setup.
 
 
 # TUTORIAL 1
@@ -80,7 +81,8 @@ var authoriseUrl = authApiUrl +
 window.location = authoriseUrl;
 ```
 
-Restart app
+Save and restart app.
+Click on "RETRIEVE MYINFO" button and login to SingPass using the credentials below.
 
 #### Login Credentials
 >UINFIN: S9812381D  
@@ -163,7 +165,7 @@ if (!_.isUndefined(params) && !_.isEmpty(params))
   request.send(params);
 ```
 
-Restart app
+Save and restart app.
 
 #### Login Credentials
 >UINFIN: S9812381D  
@@ -193,7 +195,7 @@ if (uinfin == undefined || uinfin == null) {
 }
 ```
 
-Restart app
+Save and restart app.
 
 #### Login Credentials
 >UINFIN: S9812381D  
@@ -299,7 +301,7 @@ if (!_.isUndefined(params) && !_.isEmpty(params))
   request.query(params);
 ```
 
-Restart app
+Save and restart app.
 
 #### Login Credentials
 >UINFIN: S9812381D  
@@ -325,12 +327,13 @@ var formValues = {
 };
 ```
 
-Restart app
+Save and restart app.
 
 #### Login Credentials
 >UINFIN: S9812381D  
 >Password: MyInfo2o15
 
+#### Congratulations! You have completed tutorial 2, which shows you the OAUTH 2.0 flow. You are now able to retrieve the data and populate into the form.
 
 # TUTORIAL 3
 
@@ -341,7 +344,7 @@ a. Linux/Mac OS - `start.sh`
 b. Windows - `start.bat`
 2. Uncomment L2 APIs
 3. Comment L0 APIs
-4. Save, close and restart app
+4. Save, close and Save and restart app.
 
 #### Login Credentials
 >UINFIN: S9812381D  
@@ -387,7 +390,7 @@ authHeaders = securityHelper.generateAuthorizationHeader(
 );
 ```
 
-Restart app
+Save and restart app.
 
 #### Login Credentials
 >UINFIN: S9812381D  
@@ -424,8 +427,10 @@ securityHelper.decryptJWE(jweParts[0], jweParts[1], jweParts[2], jweParts[3], jw
   })
   ```
 
-  Restart app
+  Save and restart app.
 
   #### Login Credentials
   >UINFIN: S9812381D  
   >Password: MyInfo2o15
+
+#### Congratulations! You have completed tutorial 3, which is about securing the APIs. You are now able to sign your requests and decrypt the response.
